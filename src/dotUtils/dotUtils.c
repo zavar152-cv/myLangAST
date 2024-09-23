480a93ec7d44e3391d2eef7c9e2b642865c4cdc9
Zavar30 <yarusking@mail.ru>
2024-09-23 21:06:00 +0000

    Implementation of arrays and fix of quotes bug in string

--- src/dotUtils/dotUtils.c
+++ src/dotUtils/dotUtils.c
@@ -1,57 +1,72 @@
 #include "dotUtils/dotUtils.h"
+#include <antlr3.h>
+#include <antlr3defs.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-DotNode* newDotNode(uint64_t id, const char* label, uint32_t childCount) {
-    DotNode* node = (DotNode*)malloc(sizeof(DotNode));
-    node->id = id;
-    node->label = strdup(label);
-    node->childCount = childCount;
-    node->children = (DotNode**)malloc(childCount * sizeof(DotNode*));
-    return node;
+DotNode *newDotNode(uint64_t id, const char *label, uint32_t childCount) {
+  DotNode *node = (DotNode *)malloc(sizeof(DotNode));
+  node->id = id;
+  node->label = strdup(label);
+  node->childCount = childCount;
+  node->children = (DotNode **)malloc(childCount * sizeof(DotNode *));
+  return node;
 }
 
-void destroyDotNodeTree(DotNode* root) {
-    if (root == NULL) {
-        return;
-    }
-    for (uint32_t i = 0; i < root->childCount; i++) {
-        destroyDotNodeTree(root->children[i]);
-    }
-    free(root->children);
-    free((void*)root->label);
-    free(root);
+void destroyDotNodeTree(DotNode *root) {
+  if (root == NULL) {
+    return;
+  }
+  for (uint32_t i = 0; i < root->childCount; i++) {
+    destroyDotNodeTree(root->children[i]);
+  }
+  free(root->children);
+  free((void *)root->label);
+  free(root);
 }
 
-const char* postProcessingNodeToken(const char* tokenText) {
-    if(strcmp(tokenText, "=") == 0) {
-        return "ASSIGN";
-    } else if(strcmp(tokenText, "+") == 0) {
-        return "PLUS";
-    } else if(strcmp(tokenText, "-") == 0) {
-        return "MINUS";
-    } else if(strcmp(tokenText, "*") == 0) {
-        return "MUL";
-    } else if(strcmp(tokenText, "/") == 0) {
-        return "DIV";
-    } else if(strcmp(tokenText, "==") == 0) {
-        return "EQ";
-    } else if(strcmp(tokenText, "!=") == 0) {
-        return "NEQ";
-    } else if(strcmp(tokenText, "<") == 0) {
-        return "LE";
-    } else if(strcmp(tokenText, ">") == 0) {
-        return "GR";
-    } else if(strcmp(tokenText, "<=") == 0) {
-        return "LE_EQ";
-    } else if(strcmp(tokenText, ">=") == 0) {
-        return "GR_EQ";
-    } else {
-        return tokenText;
+char* removeQuotes(const char *str) {
+    int length = strlen(str);
+    if (length > 1 && str[0] == '"' && str[length - 1] == '"') {
+        str++;
+        length -= 2;
     }
+    return strndup(str, length);
 }
 
-DotNode* preorderTraversalWithCopy(pANTLR3_BASE_TREE root, uint64_t layer, uint64_t *id, bool debug) {
+const char *postProcessingNodeToken(const char *tokenText) {
+  if (strcmp(tokenText, "=") == 0) {
+    return "ASSIGN";
+  } else if (strcmp(tokenText, "+") == 0) {
+    return "PLUS";
+  } else if (strcmp(tokenText, "-") == 0) {
+    return "MINUS";
+  } else if (strcmp(tokenText, "*") == 0) {
+    return "MUL";
+  } else if (strcmp(tokenText, "/") == 0) {
+    return "DIV";
+  } else if (strcmp(tokenText, "==") == 0) {
+    return "EQ";
+  } else if (strcmp(tokenText, "!=") == 0) {
+    return "NEQ";
+  } else if (strcmp(tokenText, "<") == 0) {
+    return "LE";
+  } else if (strcmp(tokenText, ">") == 0) {
+    return "GR";
+  } else if (strcmp(tokenText, "<=") == 0) {
+    return "LE_EQ";
+  } else if (strcmp(tokenText, ">=") == 0) {
+    return "GR_EQ";
+  } else if (strcmp(tokenText, ",") == 0) {
+    return "COMMA";
+  } else {
+    return tokenText;
+  }
+}
+
+DotNode *preorderTraversalWithCopy(pANTLR3_BASE_TREE root, uint64_t layer,
+                                   uint64_t *id, bool debug) {
   if (root == NULL) {
     return NULL;
   }
@@ -62,34 +77,54 @@ DotNode* preorderTraversalWithCopy(pANTLR3_BASE_TREE root, uint64_t layer, uint6
     printf("  ");
   }
 
-  if(debug) {
-    const char* nodeName = postProcessingNodeToken((const char*)root->getToken(root)->getText(root->getToken(root))->chars);
-    printf("node %s_%lu [label=\"%s\"]", nodeName, currentId,
-          root->getToken(root)->getText(root->getToken(root))->chars);
+  if (debug) {
+    pANTLR3_BASE_TREE parent = root->getParent(root);
+    char* tokenText = removeQuotes((const char *)root->getToken(root)
+                                             ->getText(root->getToken(root))
+                                             ->chars);
+    const char *nodeName = postProcessingNodeToken(tokenText);
+    printf("node %s_%lu [label=\"%s\"]", nodeName, currentId, tokenText);
     printf("\n");
+    free(tokenText);
   }
 
+  pANTLR3_UINT8 tokenText =
+      root->getToken(root)->getText(root->getToken(root))->chars;
+  // if(strcmp((const char*) label, "ARRAY_SIZE") == 0) {
+  //   ANTLR3_UINT32 childCount = root->getChildCount(root);
+  //   for (uint32_t i = 0; i < childCount; i++) {
+  //     root->deleteChild(root, i);
+  //   }
+  //   pANTLR3_COMMON_TOKEN token = antlr3CommonTokenNew(0);
+  //   pANTLR3_COMMON_TREE tree = antlr3CommonTreeNewFromToken(token);
+  //   root->addChild(root, tree->super);
+  // }
 
-  pANTLR3_UINT8 label = root->getToken(root)->getText(root->getToken(root))->chars;
-  DotNode* newNode = newDotNode(currentId, (const char *) label, root->getChildCount(root));
+  char* label = removeQuotes((const char*)tokenText);
+
+  DotNode *newNode =
+      newDotNode(currentId, (const char *)label, root->getChildCount(root));
+
+  free(label);
 
   for (uint32_t i = 0; i < root->getChildCount(root); i++) {
-    newNode->children[i] = preorderTraversalWithCopy((pANTLR3_BASE_TREE)root->getChild(root, i), layer + 1, 
-                      id, debug);
+    newNode->children[i] = preorderTraversalWithCopy(
+        (pANTLR3_BASE_TREE)root->getChild(root, i), layer + 1, id, debug);
   }
 
   return newNode;
 }
 
-void writeTreeToDot(FILE *file, DotNode* root) {
+void writeTreeToDot(FILE *file, DotNode *root) {
   if (root == NULL) {
     return;
   }
 
-  fprintf(file, "    node%s_%lu [label=\"%s\"]\n", postProcessingNodeToken(root->label), root->id, root->label);
+  fprintf(file, "    node%s_%lu [label=\"%s\"]\n",
+          postProcessingNodeToken(root->label), root->id, root->label);
 
   for (uint32_t i = 0; i < root->childCount; i++) {
-    DotNode* child = root->children[i];
+    DotNode *child = root->children[i];
     fprintf(file, "    node%s_%lu -> node%s_%lu;\n",
             postProcessingNodeToken(root->label), root->id,
             postProcessingNodeToken(child->label), child->id);
@@ -97,7 +132,7 @@ void writeTreeToDot(FILE *file, DotNode* root) {
   }
 }
 
-void generateDotFile(DotNode* root, const char *filename) {
+void generateDotFile(DotNode *root, const char *filename) {
   FILE *file = fopen(filename, "w");
   if (file == NULL) {
     printf("Error opening file %s for writing.\n", filename);
